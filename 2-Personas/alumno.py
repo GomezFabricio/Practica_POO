08fd5857023451945dee495c82e46207e13b07a3
Fabricio <105023101+GomezFabricio@users.noreply.github.com>
2023-08-24 22:30:20 -0300

    Delete 2-Personas directory

--- 2-Personas/alumno.py
+++
@@ -1,9 +0,0 @@
-from persona import Persona
-
-class Alumno(Persona):
-    def __init__(self, nombre, apellido, edad, cantidad_materias):
-        super().__init__(nombre, apellido, edad);
-        self.cantidad_materias = cantidad_materias;
-
-    def detalleMaterias(self):
-        return f"Cantidad Materias: {self.cantidad_materias}"
